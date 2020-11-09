e32f63e162f6715bc579e31bfb7f4b7611c7d00a
Heso113 <hampe_olsson91@hotmail.com>
2020-11-09 15:48:11 +0100

    added slick effects and fetched posts from reddit

---
+++ application.js
@@ -0,0 +1,38 @@
+let button = document.getElementById('loadButton');
+let content = document.getElementById('content');
+
+button.addEventListener('click', loadPosts);
+
+document.addEventListener("DOMContentLoaded", loadPage);
+
+function loadPage() {
+    $(document).ready(() => {
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+          });
+
+          $("#loadButton").hover(function(){
+            $(this).css("background-color", "red");
+            }, function(){
+            $(this).css("background-color", "green");
+          });
+    })
+}
+
+function loadPosts() {
+    $(document).ready(() => {
+        $(".items").animate({height: "200px"});
+        
+        $.ajax('https://www.reddit.com/r/deadbydaylight/top.json?limit=12').then(result => {
+            console.log('RESULT', result);
+            for (let i = 0; i < 12; i++) {
+                console.log(result.data.children[i].data.title);
+                console.log(result.data.children[i].data.url)
+                document.getElementById('title' + i).innerText = 'Title: ' + result.data.children[i].data.title;
+                document.getElementById('url' + i).innerText = 'URL: ' + result.data.children[i].data.url;
+            }
+        })
+    })
+}
